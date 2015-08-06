bffa906827bac4d3156f63f212572dd86ebe3c2f
Armando <flores@mtu.edu>
2015-08-06 12:15:48 -0700

    Adding in other waypoints updating animations and some styling

--- js/waypoint.js
+++ js/waypoint.js
@@ -2,6 +2,7 @@ $('.recentProjects-copy').waypoint( function(){
   $('.recentProjects-copy').addClass('active');
   $('.first-row').addClass('active');
   $('.circle-anim').addClass('active');
+  $('.scroll-down').addClass('inactive');
 },{
   offset: '45%'
 });
@@ -11,4 +12,10 @@ $('.success').waypoint( function(){
   $('.right').addClass('active');
 },{
   offset: '85%'
+});
+
+$('.footer-above').waypoint( function(){
+  $('.btn-right').addClass('active');
+},{
+  offset: '75%'
 });
